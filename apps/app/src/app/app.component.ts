05b97a5923d20cfac68d1b911d1e21d96a8001de
Nousagi Tangent <nousagi.tangent@gmail.com>
2021-03-29 19:30:26 +0800

    Add paper

--- apps/app/src/app/app.component.ts
+++ apps/app/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Color, PaperScope, Path, Point, Project } from 'paper/dist/paper-core';
 
 @Component({
   selector: 'svg-painter-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild('canvasElement', { static: true })
+  public canvasElement: ElementRef;
   title = 'app';
+
+  public ngOnInit() {
+    const scope = new PaperScope();
+    const project = new Project(this.canvasElement.nativeElement);
+    var myPath = new Path();
+    myPath.strokeColor = new Color('black');
+    myPath.add(new Point(0, 0));
+    myPath.add(new Point(100, 50));
+    project.activeLayer.addChild(myPath);
+  }
 }
